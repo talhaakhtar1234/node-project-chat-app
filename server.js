4467e1764217b9a65cf11a180995cb58757a57e5
Talha Akhtar <talhaakhtarkpr1@gmail.com>
2024-01-08 00:29:04 +0500

    commit

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const http = require('http');
 const server = http.createServer(app);
 const socketio = require('socket.io');
 const io = socketio(server);
-
+const formatMessages=require('./utils/message')
 
 const path = require('path');
 
-
+const botName='ChatCord Bot'
 
 // Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -19,17 +19,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Run when client connects
 io.on('connection', (socket) => {
     // Send a welcome message to the connected client
-    socket.emit('message', 'Welcome to ChatCord');
+    socket.emit('message', formatMessages(botName,'Welcome to ChatCord'));
 
     // Broadcast to all clients that a new user has joined the chat
-    socket.broadcast.emit('message', 'A user has joined the chat');
+    socket.broadcast.emit('message', formatMessages(botName,'A user has joined the chat'));
 
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the Chat')
+        io.emit('message', formatMessages(botName,'A user has left the Chat'))
     })
     //listen for chat messsage
     socket.on('chatMessage',msg =>{
-        io.emit('message',msg)
+        io.emit('message',formatMessages('USER',msg))
     })
 });
 
